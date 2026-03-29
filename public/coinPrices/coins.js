50a37e1281ea6878cf8e1815f7204d4feaa36dd0
JE Morray <138597153+DEADestbear@users.noreply.github.com>
2026-03-29 13:54:51 +0100

    before change
    gulp

--- public/coinPrices/coins.js
+++ public/coinPrices/coins.js
@@ -1,21 +1,37 @@
 export async function fetchBitcoinPrice() {
-    const apiKey = "CG-2UhE78yESRWdrAX3pU6fMsCZ";
     const container = document.getElementById('price');
-    
+    const apiContainer = document.getElementById('api');
     // Use the Demo URL and query parameter for consistency
-    const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&x_cg_demo_api_key=${apiKey}`;
+    const url = `https://api.coingecko.com/api/v3/simple/market_prices&x_cg_demo_api_key=${apiKey}`;
 
     try {
-        const res = await fetch(url);
-        if (!res.ok) throw new Error("Failed to fetch");
 
-        const data = await res.json();
-        
+        const params = new URLSearchParams({
+        vs_currency: 'gbp',
+        order: 'market_cap_desc',
+        per_page: '10',
+        page: '1',
+        sparkline: 'false'
+     });
+        const response = await fetch(`${API_URL}?${params}`, {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                // If using a Demo Key, uncomment the line below:
+                'x-cg-demo-api-key': 'CG-2UhE78yESRWdrAX3pU6fMsCZ'
+            }
+        });
+        if (!response.ok) throw new Error("Failed to fetch");
+
+        const data = await response.json();
+        apiContainer.innerHTML =  data;
         // data looks like: { "bitcoin": { "usd": 65000 } }
-        const price = data.bitcoin.usd;
-        
+        const bitcoinPrice = data.market_data.current_price.gbp; 
+        const ethereumPrice = data.market_data.current_price.gbp;
+
         // Update the UI inside the try block
-        container.innerHTML = `Bitcoin Price: $${price.toLocaleString()}`;
+        container.innerHTML = `Bitcoin Price: £${bitcoinPrice.toLocaleString()}<br>Ethereum Price: £${ethereumPrice.toLocaleString()}`;
+        
         console.log("Success:", data);
 
     } catch (err) {
@@ -23,4 +39,3 @@ export async function fetchBitcoinPrice() {
         container.innerHTML = "Error loading price.";
     }
 }
-
